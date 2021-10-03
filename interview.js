dfbea1242e9f829bef87b8038e1e01cd4ae653a9
felipeldn91 <felipeldn91@gmail.com>
2021-10-03 21:28:02 +0100

    performance measure

--- interview.js
+++ interview.js
@@ -14,13 +14,13 @@ function sumUp(n) {
 
 console.log(sumUp(3))
 
-// let start = 0;
-// let end = 0;
+let start = 0;
+let end = 0;
 
-// start = performance.now();
-// sumUp(5)
-// end = performance.now();
+start = performance.now();
+sumUp(5)
+end = performance.now();
 
-// perf = end - start;
+perf = end - start;
 
-// console.log(perf)
+console.log(perf)

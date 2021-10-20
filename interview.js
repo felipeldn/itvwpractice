6f16f8ead1ea12cfa91f4eaa530074167841f8f2
felipeldn91 <felipeldn91@gmail.com>
2021-10-20 12:44:18 +0100

    anagrams

--- interview.js
+++ interview.js
@@ -128,3 +128,16 @@ function getRaisedGrades(grades) {
 
   return raisedGrades
 }
+
+//compare strings to find anagrams
+
+function stringAnagrams(a, b) {
+  let y = a.split("").sort().join(""),
+      z = b.split("").sort().join("");
+  
+  console.log(
+    z === y 
+    ? a + " and " + b + " are anagrams! "
+    : a + " and " + b + " are not anagrams! "
+  )
+}
